ad451bf22ac890aa214ce54c0c0a577ed49eccfd
Ashik Ghosh <ashikghosh763@gmail.com>
2021-11-05 23:44:05 +0600

    js started

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+var req = new XMLHttpRequest();
+req.open('GET', 'https://jsonplaceholder.typicode.com/posts',true);
+req.send();
+req.on
+
+
+
+let blackjackGame = {
+    'you': {'scoreSpan' : '#yourScore' ,'div': '#me','score': 0},
+    'dealer': {'scoreSpan' : '#dealerScore' ,'div': '#dealer','score': 0},
+
+}
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+const hitSound = new Audio('./sounds/swish.m4a');
+
+document.querySelector(".hit").addEventListener("click", blackjackHit)
+
+function blackjackHit() {
+    let cardImage = document.createElement('img');
+    cardImage.src = './images/2.png';
+    document.querySelector("#my-cards").appendChild(cardImage);
+    hitSound.play();
+ }
